e06d4e31abfd6ae3e7166a2682942ba9ad516d2c
Patrycja Brzeskot <patrycja.brzeskot97@gmail.com>
2020-06-22 11:24:37 +0200

    contact

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -1,27 +1,20 @@
 import React from "react"
 import styled from "styled-components"
+import { Icon } from "semantic-ui-react"
+import "semantic-ui-css/semantic.min.css"
 
 const Container = styled.div`
   ${"" /* ${"" /* min-height: 400px; */} */}
-  line-height: 100px;
+  line-height: 50px;
   text-align: center;
   padding: 50px;
   margin-left: 100px;
   margin-right: 100px;
 `
 
-const Inner = styled.div`
-  ${"" /* ${"" /* min-height: 400px; */} */}
-  line-height: 100px;
-  text-align: center;
-  border: 1px solid #000;
-  padding: 50px;
-`
-
 const Text = styled.span`
   display: inline-block;
   vertical-align: middle;
-  line-height: normal;
   font-size: 25px;
   max-width: 50%;
 `
@@ -30,12 +23,13 @@ const Contact = () => {
   return (
     <>
       <Container>
-        <Inner>
-          <Text>
-            kontakt.kingabrzeskot@gmail.com
-            <br />
-          </Text>
-        </Inner>
+        <Text>
+          <Icon name="envelope" /> kingabrzeskot@gmail.com
+        </Text>
+        <br />
+        <Text>
+          <Icon name="phone" /> 530 819 891
+        </Text>
       </Container>
     </>
   )
