8087114b488709a7a4f57828b9f474c0775446f9
Patrycja Brzeskot <patrycja.brzeskot97@gmail.com>
2020-06-23 16:18:02 +0200

    styling

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 import { useScrollPosition } from "../hooks/useScrollPosition"
 import scrollTo from "gatsby-plugin-smoothscroll"
@@ -7,26 +7,25 @@ import LanguageSwitch from "./LanguageSwitch"
 import { I18nProvider, withI18n, Trans } from "@lingui/react"
 import { navigateTo } from "gatsby-link"
 import { useLocation, useContext } from "react-router"
+import "./sections/quote.css"
 
 const Container = styled.div`
-  // box-shadow: 0px 0px 5px #c9c3c3;
   overflow: hidden;
-  background-color: rgba(255, 255, 255, 0);
   top: 0;
   width: 100%;
   position: fixed !important;
   overflow: "hidden";
   z-index: 99;
   height: 50px;
+  color: white;
 `
 
 const MenuItem = styled.li`
   width: 25%;
   cursor: pointer;
-  text-shadow: 1px 1px 8px #fff;
   transition: letter-spacing 0.4s;
   &:hover {
-    letter-spacing: 2px;
+    letter-spacing: 1px;
   }
 `
 const ItemsWrapper = styled.ul`
@@ -39,35 +38,35 @@ const ItemsWrapper = styled.ul`
 `
 
 const Logo = styled.p`
-  width: 500px;
   display: flex;
   float: left;
   margin-left: 30px;
-  font-weight: bold;
-  margin-top: 10px;
+  padding-top: 10px;
+  font-size: 25px;
+  font-family: LovedbytheKing-Regular;
 `
-
-// const Slider = styled.hr`
-//   color: ${({ theme }) => theme.colors.heading};
-//   width: 15%;
-//   height: 2px;
-//   margin: 5px 0 0 0;
-//   transition: margin 0.5s;
-//   margin-left: ${({ position }) => {
-//     switch (position) {
-//       case "home":
-//         return "0"
-//       case "about":
-//         return "25%"
-//       case "projects":
-//         return "50%"
-//       case "contact":
-//         return "75%"
-//     }
-//   }};
-// `
+//const [position, setPosition] = useState["top"]
 
 const Menu = props => {
+  // let listener
+
+  // const [position, setPosition] = useState["top"]
+
+  // const componentDidMount = () => {
+  //   listener = document.addEventListener("scroll", e => {
+  //     let scrolled = document.scrollingElement.scrollTop
+  //     if (scrolled >= 120) {
+  //       if (position !== "amir") {
+  //         setPosition("amir")
+  //       }
+  //     } else {
+  //       if (position !== "top") {
+  //         setPosition("top")
+  //       }
+  //     }
+  //   })
+  // }
+
   // let location = useLocation()
   // console.log(location)
 
@@ -82,29 +81,19 @@ const Menu = props => {
   //   navigateTo(prefix(lang))
   // }
   return (
-    <Container>
+    <Container style={{ backgroundColor: "black" }}>
       {/* <LanguageSwitch lang={props.language} onLangClick={onLangChange} /> */}
 
       <Logo> KINGA BRZESKOT</Logo>
       <ItemsWrapper>
-        {/* ref={refs.menu} */}
-        <MenuItem onClick={() => scrollTo("#about-section")}>
-          <Trans>About me</Trans>
-        </MenuItem>
-        <MenuItem onClick={() => scrollTo("#skills-section")}>
-          <Trans>Skills</Trans>
-        </MenuItem>
-        <MenuItem onClick={() => scrollTo("#opimions-section")}>
-          <Trans>Opinions</Trans>
-        </MenuItem>
-        <MenuItem onClick={() => scrollTo("#contact-section")}>
-          <Trans>Contact</Trans>
-        </MenuItem>
+        {/* <MenuItem onClick={() => scrollTo("#about-section")}> */}
+        <MenuItem>O mnie</MenuItem>
+        <MenuItem>Umiejętności</MenuItem>
+        <MenuItem>Opinie</MenuItem>
+        <MenuItem>Kontakt</MenuItem>
       </ItemsWrapper>
-      {/* <Slider position={scrollPosition} ref={refs.slider} /> */}
     </Container>
   )
 }
 
-// export default withI18n()(Menu)
 export default Menu
